9d9d7e68634da09d5ce3ab656030e4af3e6b91d3
Angel <angelo_stoyanov@abv.bg>
2022-12-20 15:18:20 +0200

    header footer nav errorboundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import Home from './components/Home/Home';
 import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
 import Trips from './components/Trips/Trips';
+import Header from './components/Header/Header';
+import ErrorBoundary from './utils/ErrorBoundary';
+import Footer from './components/Footer/Footer';
+import Logout from './components/Logout/Logout';
 
 
+
+const Layout = () => (
+  <>
+    <Header />
+    <Outlet />
+    <Footer />
+  </>
+)
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/register',
-    element: <Register />
-  }, {
-    path: '/trips',
-    element: <Trips />
-  },
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      },
+      {
+        path: '/trips',
+        element: <Trips />
+      },
+      {
+        path:'/logout',
+        element:<Logout/>
+      }
+    ]
+  }
+
+
 ])
 
 
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <>
+      <ErrorBoundary>
+
+        <RouterProvider router={router} />
+      </ErrorBoundary>
+    </>
 
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+import './Header.css'
+
+
+export default function Header() {
+
+
+
+    return (
+       
+            <header className="header">
+                <nav className="nav">
+                    <li className="nav-li"><Link to={'/'}>Home</Link></li>
+                    <li className="nav-li"><Link to={'/trips'}>Trips</Link></li>
+                    <li className="nav-li"><Link to={'/login'}>Login</Link></li>
+                    <li className="nav-li"><Link to={'/register'}>Register</Link></li>
+                    <li className="nav-li"><Link to={'/logout'}>Logout</Link></li>
+                </nav>
+            </header>
+       
+    )
+}
