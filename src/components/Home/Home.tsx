c794f1fb9e71bb1b700ee36e8c60af1c23ce8191
Angel <angelo_stoyanov@abv.bg>
2023-02-21 12:38:43 +0200

    css media

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -13,11 +13,11 @@ function Home() {
         <>
             <Grid container sx={{ justifyContent: 'center', bgcolor: '#cfe8fc', padding: '30px', minHeight: '100vh' }} spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                 <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding:'15px' }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding:'25px' }}>
                         <h1>Welcome TRAVELERS or future TRAVELERS!</h1>
-                        <h1>This is an APP where you can share your trips or get valuable tips for your future trips.</h1>
+                        <h4>This is an APP where you can share your trips or get valuable tips for your future trips.</h4>
                        {(trips!==undefined && trips.length>0)?
-                        <h1>These are our TOP 5 most liked TRIPS!</h1>
+                        <h4>These are our TOP 5 most liked TRIPS!</h4>
                        :''}
                     </Box>
                     <Box sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,7 +1,6 @@
 
 
 const API_URL = 'https://www.api-hack-trip.com'
-// const API_URL = 'http://185.228.26.119'
 
 
 export const CONNECTIONURL = API_URL
