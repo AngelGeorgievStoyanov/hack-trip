9113620e768bb63992966e09b54f2cb95b2b6646
Angel <angelo_stoyanov@abv.bg>
2023-06-09 18:05:38 +0300

    forgotPassword page magic string

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, keyframes, useMediaQuery } from '@mui/material';
+import { Box, Grid, useMediaQuery } from '@mui/material';
 import { LoginContext } from '../../App';
 import { Trip } from '../../model/trip';
 import TripList from '../Trips/TripsList/TripsList';
@@ -16,7 +16,6 @@ type decode = {
 
 
 const API_TRIP: ApiTrip<IdType, Trip> = new tripService.ApiTripImpl<IdType, Trip>('data/trips');
-
 
 
 
